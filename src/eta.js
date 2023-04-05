63666556cf47f3e39931ed7365bce89b9d11c037
ocinpp <ocin25@gmail.com>
2023-04-06 01:26:45 +0800

    Refactor and add NLB module

---
+++ src/eta.js
@@ -0,0 +1,18 @@
+import etaKmb from "./eta-kmb";
+import etaNlb from "./eta-nlb";
+
+export function getEtaByCompany(company) {
+  if (company === "KMB") {
+    return etaKmb;
+  } else if (company === "NLB") {
+    return etaNlb;
+  } else {
+    console.error("Not supported!");
+  }
+}
+
+// extract bus number at start so that the suffix is removed
+export function extractNumber(route) {
+  const num = route.replace(/[^0-9]/g, "");
+  return parseInt(num != null ? num : route);
+}
