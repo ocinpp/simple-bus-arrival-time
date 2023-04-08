bf1282141d65316b4e47624a456ee8c2c17e83c9
ocinpp <ocin25@gmail.com>
2023-04-08 17:36:25 +0800

    Add new functions for settings

--- src/eta-kmb.js
+++ src/eta-kmb.js
@@ -1,9 +1,57 @@
 import moment from "moment";
 
 const etaKmb = {
+  async getRoutes() {
+    let res = null;
+    const url = `https://data.etabus.gov.hk/v1/transport/kmb/route/`;
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data["generated_timestamp"]);
+      res = data.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+    return res;
+  },
+
+  async getStops() {
+    let res = null;
+    const url = `https://data.etabus.gov.hk/v1/transport/kmb/stop/`;
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data["generated_timestamp"]);
+      res = data.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+    return res;
+  },
+
   async getRoute(route, dir, serviceType) {
     let res = null;
     const url = `https://data.etabus.gov.hk/v1/transport/kmb/route/${route}/${dir}/${serviceType}`;
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data["generated_timestamp"]);
+      res = data.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+    return res;
+  },
+
+  async getRouteStops(route, dir, serviceType) {
+    let res = null;
+    const url = `https://data.etabus.gov.hk/v1/transport/kmb/route-stop/${route}/${dir}/${serviceType}`;
 
     try {
       const response = await fetch(url);
@@ -33,7 +81,7 @@ const etaKmb = {
     return res;
   },
 
-  async getEtas(busStop, route, dir, lang) {
+  async getEtas(busStop, route, dir, serviceType, lang) {
     let res = [];
     const url = `https://data.etabus.gov.hk/v1/transport/kmb/stop-eta/${busStop}`;
 
@@ -50,12 +98,13 @@ const etaKmb = {
       (o) =>
         o.route === route &&
         dir.toUpperCase().startsWith(o.dir.toUpperCase()) &&
+        serviceType === o.service_type + "" &&
         !!o.eta
     );
   },
 
-  async getEtaDisplay(busStop, route, dir, lang) {
-    const etas = await this.getEtas(busStop, route, dir, lang);
+  async getEtaDisplay(busStop, route, dir, serviceType, lang) {
+    const etas = await this.getEtas(busStop, route, dir, serviceType, lang);
     for (const eta of etas) {
       // format the eta time
       if (!!eta.eta) {
