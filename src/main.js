607006f5021b726f6f7d047c0ae4d099280b4a28
ocinpp <ocin25@gmail.com>
2024-11-23 20:49:09 +0800

    Call injectSpeedInsights

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from "vue";
 import { inject } from "@vercel/analytics";
+import { injectSpeedInsights } from "@vercel/speed-insights";
 import "tailwindcss/tailwind.css";
 import "./style.css";
 import App from "./App.vue";
 
 inject();
+injectSpeedInsights();
 createApp(App).mount("#app");
